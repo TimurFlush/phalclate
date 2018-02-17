d9af1f5b233b71e06bf8e22e1780eb1262616103
Timur Flush <flush02@yandex.ru>
2018-02-18 03:18:29 +0500

    Full refactor of the phpDoc in StorageInterface.php

--- src/StorageInterface.php
+++ src/StorageInterface.php
@@ -5,10 +5,14 @@ namespace TimurFlush\Phalclate;
 /**
  * Class StorageInterface
  * @package TimurFlush\Phalclate
+ * @author Timur Flush
+ * @version 1.0.2
  */
 interface StorageInterface
 {
     /**
+     * Push data to storage.
+     *
      * @param $key
      * @param $value
      * @return mixed
@@ -16,24 +20,32 @@ interface StorageInterface
     public function save($key, $value) : void;
 
     /**
+     * Get data from storage.
+     *
      * @param null $key
      * @return mixed|\stdClass
      */
     public function get($key = null);
 
     /**
+     * Checks for existence of data in storage.
+     *
      * @param $key
      * @return mixed
      */
     public function exists($key) : bool;
 
     /**
+     * Removes data from storage.
+     *
      * @param $key
      * @return void
      */
     public function remove($key) : void;
 
     /**
+     * Clears storage.
+     *
      * @return void
      */
     public function flush() : void;
