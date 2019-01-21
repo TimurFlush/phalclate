eb8b31709b96e92b64f2f325ea8da7cd89dd6f3d
Timur Flush <flush02@tutanota.com>
2019-01-21 23:50:35 +0500

    Bug in translation fixtures was fixed.

--- tests/_fixtures/translations.php
+++ tests/_fixtures/translations.php
@@ -2,24 +2,28 @@
 
 return [
     [
+        'id' => 1,
         'key' => 'color',
         'language' => 'en',
         'dialect' => 'US',
         'value' => 'color'
     ],
     [
+        'id' => 2,
         'key' => 'color',
         'language' => 'en',
         'dialect' => 'GB',
         'value' => 'colour'
     ],
     [
+        'id' => 3,
         'key' => 'position',
         'language' => 'en',
         'dialect' => 'US',
         'value' => 'center'
     ],
     [
+        'id' => 4,
         'key' => 'position',
         'language' => 'en',
         'dialect' => 'GB',
