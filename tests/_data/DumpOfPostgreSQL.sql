63025a4dbd81451ebd9c9f685d46f737601a3489
Timur Flush <flush02@tutanota.com>
2019-01-21 23:59:25 +0500

    Postgresql dump was refactored.

--- tests/_data/DumpOfPostgreSQL.sql
+++ tests/_data/DumpOfPostgreSQL.sql
@@ -1,91 +1,10 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 11.1
--- Dumped by pg_dump version 11.1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: translations; Type: TABLE; Schema: public; Owner: postgres
---
 DROP TABLE IF EXISTS public.translations;
 CREATE TABLE public.translations (
-    id integer NOT NULL,
+    id serial PRIMARY KEY,
     key character varying(255) NOT NULL,
     language character(2) NOT NULL,
     dialect character varying(255),
     value text NOT NULL
 );
 
-
 ALTER TABLE public.translations OWNER TO postgres;
-
---
--- Name: translations_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.translations_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.translations_id_seq OWNER TO postgres;
-
---
--- Name: translations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.translations_id_seq OWNED BY public.translations.id;
-
-
---
--- Name: translations id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.translations ALTER COLUMN id SET DEFAULT nextval('public.translations_id_seq'::regclass);
-
-
---
--- Data for Name: translations; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.translations (id, key, language, dialect, value) FROM stdin;
-\.
-
-
---
--- Name: translations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.translations_id_seq', 1, false);
-
-
---
--- Name: translations translations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.translations
-    ADD CONSTRAINT translations_pkey PRIMARY KEY (id);
-
---
--- PostgreSQL database dump complete
---
-
